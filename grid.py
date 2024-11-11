398059315cb6f83270e8f1c73ab14c93efc969eb
Mahdi Rahman <154563821+MahdiRahman4@users.noreply.github.com>
2024-11-11 14:22:08 -0500

    Add files via upload

--- grid.py
+++ grid.py
@@ -1,6 +1,6 @@
 def build_grid(commandLineInput):
     grid = [[0] * commandLineInput for _ in range(commandLineInput)]
+    return grid
+def print_grid(grid):
     for row in grid:
         print(row)
-    
-    return grid
