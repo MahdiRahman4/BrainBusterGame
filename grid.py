ac56aef124b4a019f5e15b67631e1cc75455d606
MahdiRahman4 <maahdi.raahmaan@gmail.com>
2024-11-13 23:37:27 -0500

    .

---
+++ grid.py
@@ -0,0 +1,41 @@
+import random
+class Grid:
+    
+    
+    def __init__(self,size):
+        self.size = size
+        self.grid= [['X'] * size for _ in range(size)]
+        self.hidden_grid=self.make_hidden_grid()
+        
+    def make_hidden_grid(self):
+        hidden_grid= [[None]* self.size for _ in range(self.size)]
+        index=0
+        temp_list= [0]*(self.size**2)
+        for i in range(0, len(temp_list), 2):
+            temp  = random.randint(0,9)
+            temp_list[i],temp_list[i+1]=temp,temp
+        random.shuffle(temp_list)
+        for j in range(self.size):
+            for r in range(self.size):
+                hidden_grid[j][r]=temp_list[index]
+                index+=1
+        return hidden_grid
+
+    def display(self):
+        # Print column headers (e.g., A B C ...)
+        print("   " + " ".join(chr(65 + i) for i in range(self.size)))
+        
+        # Print each row with row numbers
+        for idx, row in enumerate(self.grid):
+            print(f"{idx}  " + " ".join(map(str, row)))
+    def display_hidden(self):
+        # Print column headers (e.g., A B C ...)
+        print("   " + " ".join(chr(65 + i) for i in range(self.size)))
+        
+        # Print each row with row numbers
+        for idx, row in enumerate(self.hidden_grid):
+            print(f"{idx}  " + " ".join(map(str, row)))
+    def reveal_number(self,row,col):
+        self.grid[row][col] = self.hidden_grid[row][col]
+    def hide_number(self,row,col):
+        self.grid[row][col] = 'X'
