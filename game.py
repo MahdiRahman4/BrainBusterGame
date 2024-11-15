93da447c26ae2cbd47829abd5e3777f927be0295
Mahdi Rahman <154563821+MahdiRahman4@users.noreply.github.com>
2024-11-15 00:20:18 -0500

    Add files via upload
    how am i not done with this yet smh

--- game.py
+++ game.py
@@ -3,21 +3,120 @@ import random
 import os
 import time
 import string
-from grid import build_grid, print_grid
+from grid import *
+cell1=None
+cell2=None
+permanent_cell=None
+guess_count=0
 def starter_grid():
     if len(sys.argv) != 2 or sys.argv[1] not in ['2', '4', '6']:
         sys.stdout.write("Invalid grid size. Please specify 2, 4, or 6.\n")
         sys.exit(1)
-    grid_size=int(sys.argv[1])
-    grid= build_grid(grid_size)
-    print_grid(grid)
+    size= int(sys.argv[1])
+    grid=Grid(size)
+    return grid
 def title():
     print("----------------------")
     print("|    Brain Buster    |")
-    print("----------------------")
+    print("----------------------\n")
+def options():
+    print("\n1. Let me select two elements\n2. Uncover one element for me\n"+
+          "3. I give up - reveal the grid\n4. New game\n5. Exit\n")
+def selection_switch_case(grid):
+    while True: 
+        try:
+            selection = int(input("Select:"))
+        except ValueError:
+            print("Please1 input a number")
+            continue
+        else:
+            break
+    while (selection !=1 and selection !=2 
+        and selection!=3 and selection!=4 and selection!=5):
+            print("Invalid input. Try again")
+            selection = input("Select: ")
+    if (selection==1):
+            cell1, cell2 = guess(grid)
+            if (grid.hidden_grid[ord(cell1[0])-97][int(cell1[1])] == grid.hidden_grid[ord(cell2[0])-97][int(cell2[1])] ):
+                grid.reveal_number(ord(cell1[0])-97,int(cell1[1]))
+                grid.reveal_number(ord(cell2[0])-97,int(cell2[1]))
+                clear(grid)
+            else:
+                grid.reveal_number(ord(cell1[0])-97,int(cell1[1]))
+                grid.reveal_number(ord(cell2[0])-97,int(cell2[1]))
+                grid.display()
+                time.sleep(2)
+                grid.hide_number(ord(cell1[0])-97,int(cell1[1]))
+                grid.hide_number(ord(cell2[0])-97,int(cell2[1]))
+                clear(grid)
+    elif (selection==2):
+            permanent_cell=uncover(grid)
+            grid.reveal_number(ord(permanent_cell[0])-97,int(permanent_cell[1]))
+            clear(grid)
+    elif(selection==3):
+            grid.display_hidden()
+            sys.exit(1)
+    elif(selection==5):
+            sys.exit(1)
+def guess(grid):
+    global guess_count
+    cell1 =input("Enter cell coordinates (e.g., a0): ") 
+    
+    while not cell1[1:2].isdigit():
+        print("Input error: row must be a number. Please try again.")
+        cell1 = input("Enter cell coordinates (e.g., a0): ")
+        
+    while(cell1 [0:1]>chr(102) or cell1 [0:1]<chr(97)):
+        print("Input error: column entry is out of range for this grid. Please try again.")
+        cell1 =input("Enter cell coordinates (e.g., a0): ")
+        
+    while(int(cell1[1:2])>5 or int(cell1[1:2])<0):
+        print("Input error: row entry is out of range for this grid. Please try again.")
+        cell1 =input("Enter cell coordinates (e.g., a0): ")
+        
+    cell2 = input("Enter cell coordinates (e.g., a0): ")
+    
+    while not cell2[1:2].isdigit():
+        print("Input error: row must be a number. Please try again.")
+        cell2 = input("Enter cell coordinates (e.g., a0): ")
+
+    while(int(cell2[1:2])>5 or int(cell2[1:2])<0):
+        print("Input error: row entry is out of range for this grid. Please try again.")
+        cell2 =input("Enter cell coordinates (e.g., a0): ")
+    guess_count+=1
+    return cell1,cell2
+def uncover(grid):
+    global guess_count
+    permanent_cell =input("Enter cell coordinates (e.g., a0): ") 
+    
+    while not permanent_cell[1:2].isdigit():
+        print("Input error: row must be a number. Please try again.")
+        permanent_cell = input("Enter cell coordinates (e.g., a0): ")
+        
+    while(permanent_cell [0:1]>chr(102) or permanent_cell [0:1]<chr(97)):
+        print("Input error: column entry is out of range for this grid. Please try again.")
+        permanent_cell =input("Enter cell coordinates (e.g., a0): ")
+        
+    while(int(permanent_cell[1:2])>5 or int(permanent_cell[1:2])<0):
+        print("Input error: row entry is out of range for this grid. Please try again.")
+        permanent_cell =input("Enter cell coordinates (e.g., a0): ")
+        guess_count+=2
+    return permanent_cell
+def always_on_screen(grid):
+    title()
+    grid.display()
+    options()
+    selection_switch_case(grid)
+def clear(grid):
+    os.system('cls')
+    always_on_screen(grid)
+    
 def main():
     title()
-    starter_grid()
-
+    grid=starter_grid()
+    grid.display()
+    options()
+    selection_switch_case(grid)
+    
 if __name__ == "__main__":
     main()
--- grid.py
+++ grid.py
@@ -1,6 +1,38 @@
-def build_grid(commandLineInput):
-    grid = [[0] * commandLineInput for _ in range(commandLineInput)]
-    return grid
-def print_grid(grid):
-    for row in grid:
-        print(row)
+import random
+class Grid:
+    
+    
+    def __init__(self,size):
+        self.size = size
+        self.grid= [['X'] * size for _ in range(size)]
+        self.hidden_grid=self.make_hidden_grid()
+        
+    def make_hidden_grid(self):
+        hidden_grid= [[None]* self.size for _ in range(self.size)]
+        index=0
+        temp_list= [0]*(self.size**2)
+        for i in range(0, len(temp_list), 2):
+            temp  = random.randint(0,9)
+            temp_list[i],temp_list[i+1]=temp,temp
+        random.shuffle(temp_list)
+        for j in range(self.size):
+            for r in range(self.size):
+                hidden_grid[j][r]=temp_list[index]
+                index+=1
+        return hidden_grid
+
+    def display(self):
+        print("   " + " ".join(chr(65 + i) for i in range(self.size)))
+        for idx, row in enumerate(self.grid):
+            print(f"{idx}  " + " ".join(map(str, row)))
+            
+    def display_hidden(self):
+        print("   " + " ".join(chr(65 + i) for i in range(self.size)))
+        for idx, row in enumerate(self.hidden_grid):
+            print(f"{idx}  " + " ".join(map(str, row)))
+
+    def reveal_number(self,row,col):
+        self.grid[col][row] = self.hidden_grid[col][row]
+
+    def hide_number(self,row,col):
+        self.grid[col][row] = 'X'
